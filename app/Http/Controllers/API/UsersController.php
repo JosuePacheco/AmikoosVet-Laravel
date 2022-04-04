709790859b71fc04c0ea99435462227b82bcd5fd
BisaOrtiz <brisa.ortiz@gmail.com>
2022-04-03 22:28:51 -0500

    API usuarios

---
+++ app/Http/Controllers/API/UsersController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return UserrResource::collection(User::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $request->validate(User::reglasValidacion());
+        $user = User::create($request->all());
+        return new UserResource($user);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new UserrResource(User::findOrFail($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate(User::reglasValidacion());
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,45 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
     protected $fillable = [
         'name',
+        'codigopostal',
+        'direccion',
+        'telefono'
+        'nickname'
         'email',
         'password',
     ];
+
+    public static function reglasValidacion() {
+        // https://laravel.com/docs/9.x/validation#available-validation-rules
+        return [
+            'name' => 'required|string|min:0'
+            , 'codigopostal' => 'required|numeric|min:3|max:6'
+            , 'direccion' => 'required|string|min:0|max:2000'
+            , 'telefono' => 'required|numeric|min:0|max:12'
+            , 'nickname' => 'required|string|min:0|max:50'
+            , 'email' => 'required|string|min:0|max:100'
+            , 'password' => 'required|string|min:6'
+        ];
+    }
+
+    public static function etiquetas() {
+        return [
+            'name' => 'name'
+            , 'codigopostal' => 'codigopostal'
+            , 'direccion' => 'direccion'
+            , 'telefono' => 'telefono'
+            , 'nickname' => 'nickname'
+            , 'email' => 'email'
+            , 'password' => 'password'
+        ];
+    }
 
     /**
      * The attributes that should be hidden for serialization.
