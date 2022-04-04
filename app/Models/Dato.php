33bae4c0aa7194907f3e2d53a70634f06e81c874
BisaOrtiz <brisa.ortiz@gmail.com>
2022-04-03 19:07:45 -0500

    API

--- app/Models/Dato.php
+++ app/Models/Dato.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Dato extends Model
 {
-    use HasFactory;
+    protected $table = 'datos';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'venta_id'
+        , 'cantidad'
+        , 'total'
+    ];
+
+    public static function reglasValidacion() {
+        // https://laravel.com/docs/9.x/validation#available-validation-rules
+        return [
+            'venta_id' => 'required|integer|min:0'
+            , 'cantidad' => 'required|numeric|min:1'
+            , 'total' => 'required|numeric|min:0|max:200'
+        ];
+    }
+
+    public static function etiquetas() {
+        return [
+            'venta_id' => 'venta'
+            , 'cantidad' => 'cantidad'
+            , 'total' => 'total'
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ProveedoresController;
+use App\Http\Controllers\API\ProductosController;
+use App\Http\Controllers\API\VentasController;
+use App\Http\Controllers\API\DatosController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,38 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::APIresource('proveedores', ProveedoresController::class)->names('api-proveedores');
+    Route::APIresource('productos', ProductosController::class)->names('api-productos');
+    Route::APIresource('ventas', VentasController::class)->names('api-ventas');
+    Route::APIresource('datos', DatosController::class)->names('api-datos');
+    
+});
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email'
+        , 'password' => 'required'
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken('app')->plainTextToken;
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
