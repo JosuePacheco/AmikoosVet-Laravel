a7b8212e2e434407a4507c1aacc700916232d1a0
JosuePacheco <al222010785@gmail.com>
2022-04-06 15:19:10 -0500

    Cambios Api

--- app/Http/Controllers/API/DatosController.php
+++ app/Http/Controllers/API/DatosController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\DatoResource;
 use App\Models\Dato;
+use App\Http\Controllers\ApiController;
 
 class DatosController extends Controller
 {
